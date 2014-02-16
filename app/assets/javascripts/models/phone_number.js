fc6cd305da5fdb4d5e1620c2330811b60034665d
Wojciech Ryrych <wojtek@ryrych.pl>
2014-02-16 17:09:56 +0100

    rewrite phone number model to coffee

--- app/assets/javascripts/models/phone_number.js
+++
@@ -1,4 +0,0 @@
-App.PhoneNumber = DS.Model.extend({
-  number:  DS.attr('string'),
-  contact: DS.belongsTo('App.Contact')
-});
---
+++ app/assets/javascripts/models/phone_number.js.coffee
@@ -0,0 +1,3 @@
+App.PhoneNumber = DS.Model.extend
+  number:  DS.attr 'string'
+  contact: DS.belongsTo 'App.Contact'
